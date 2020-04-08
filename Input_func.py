c3dfc70b5b9ad7ab8f2bb553069e1910114ad240
patrick shapard <pshapard@outlook.com>
2020-04-07 22:33:02 -0500

    removed import statements, remove indentation

--- Input_func.py
+++ Input_func.py
@@ -1,29 +1,31 @@
-import sys
-import getpass
-
 
 
 def OutPutToCountryLog():
     """Open and reads main log file, TestResults.txt
-       Searches main log file and retrieves data for each country
-       and outputs the data to each country's log file """
+    Searches main log file and retrieves data for each country
+    and outputs the data to each country's log file """
     filename = 'TestResults.txt'
-    
-    output_file = {'World':'logfile_world.txt',
-                   'Us':'logfile_usa.txt',
-                   'France':'logfile_france.txt',
-                   'Spain':'logfile_spain.txt',
-                   'Germany':'logfile_germany.txt',
-                   'Uk':'logfile_uk.txt',
-                   'Iran':'logfile_iran.txt',
-                   'Italy':'logfile_italy.txt'}
-    
+    output_file = {'World': 'logfile_world.txt',
+                 'Us': 'logfile_usa.txt',
+                 'France': 'logfile_france.txt',
+                 'Spain': 'logfile_spain.txt',
+                 'Germany': 'logfile_germany.txt',
+                 'Uk': 'logfile_uk.txt',
+                 'Iran': 'logfile_iran.txt',
+                 'Italy': 'logfile_italy.txt'}
+
     for string, logfile in output_file.items():
-        with open(filename) as f:
-            datafile = f.readlines()
-            string_found = [line for line in datafile if string in line]
-            with open(logfile, 'w') as inf:
-                for line in string_found:
-                    inf.write(line)
+      with open(filename) as f:
+        datafile = f.readlines()
+        string_found = [line for line in datafile if string in line]
+        with open(logfile, 'w') as inf:
+            for line in string_found:
+                inf.write(line)
 
 
+def main():
+    OutPutToCountryLog()
+
+
+if __name__ == '__main__':
+    main()
