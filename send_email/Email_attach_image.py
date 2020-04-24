89af6cbf5a29e054ffa3fb4805981d236fb424dd
patrick shapard <pshapard@outlook.com>
2020-04-24 14:42:11 -0500

    Added notes throughout script for better understanding

--- send_email/Email_attach_image.py
+++ send_email/Email_attach_image.py
@@ -1,5 +1,6 @@
 # Author: Patrick Shapard
 # Created: 04/22/2020
+#updated: 04/24/2020
 # Python script to send emails with and without attachments
 
 # If you don't have an email account to use, issue the command below to run on your local host.
@@ -13,9 +14,13 @@
 import os
 import smtplib
 import imghdr
+from email.message import EmailMessage
 from Email_variables import EMAIL_ADDRESS, EMAIL_PASSWORD, EMAIL_SERVER, TO_RECEIVER, CC_RECEIVER, email_body, email_subject
 
-files = ['image file to attach. example:  C:\\Folder\\image.jpg']
+files = ['image file to attach. example:  C:\\Folder\\image.jpg'] 
+"""if you want to send multiple image files, create a list with folder/image.
+    example: files = ['c:\\folder1\image1.jpg', c:\\folder2\image2.png]"""
+
 
 def EmailConstructor():
     """Function to construct email message with attachment and send it. """
@@ -23,14 +28,15 @@ def EmailConstructor():
     msg['Subject'] = email_subject
     msg['From'] = EMAIL_ADDRESS
     msg['To'] = TO_RECEIVER
-    msg['Cc'] = CC_RECEIVER
+    msg['Cc'] = CC_RECEIVER  #The Cc field is not required. Added as an example.  You can also add a Bcc field.
     msg.set_content(email_body)
     
     for file in files:
         with open(file, 'rb') as f:
             data_file = f.read()
+            file_type = imghdr.what(f.name)
     
-         msg.add_attachment(data_file, maintype='image', subtype=file_type, filename=image.jpg'])
+         msg.add_attachment(data_file, maintype='image', subtype=file_type, filename=image.jpg)
     
     with smtplib.SMTP_SSL(EMAIL_SERVER, 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
