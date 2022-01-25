6e5350c9925e9d4e3d1d81fe763cdc08efc8d3f3
Patrick Shapard <pshapard@outlook.com>
2022-01-25 12:43:11 -0600

    additinal changes

--- real_python_courses/Email_variables.py
+++ real_python_courses/Email_variables.py
@@ -3,9 +3,9 @@
 # Created: 04/22/2020
 #variables used for email script
 
-TO_RECEIVER = 'pshapard@outlook.com'
-CC_RECEIVER =  'shapardpatrick@gmail.com'
-EMAIL_ADDRESS = 'shapardpatrick@gmail.com'
+TO_RECEIVER = put receiver address
+CC_RECEIVER =  put sender email address
+EMAIL_ADDRESS = put sender email address
 EMAIL_PASSWORD = put your password here
 EMAIL_SERVER = put your email server here
 email_body = 'My updated resume is attached. \n\n\nRegards,\nPatrick'
