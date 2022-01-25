7266fe102359c8676d2832525bcebf39a8462220
Patrick Shapard <pshapard@outlook.com>
2022-01-25 12:40:50 -0600

    removed security isses

--- real_python_courses/Email_variables.py
+++ real_python_courses/Email_variables.py
@@ -6,7 +6,7 @@
 TO_RECEIVER = 'pshapard@outlook.com'
 CC_RECEIVER =  'shapardpatrick@gmail.com'
 EMAIL_ADDRESS = 'shapardpatrick@gmail.com'
-EMAIL_PASSWORD = 'Tr0jans7'
-EMAIL_SERVER = 'smtp.gmail.com'
+EMAIL_PASSWORD = put your password here
+EMAIL_SERVER = put your email server here
 email_body = 'My updated resume is attached. \n\n\nRegards,\nPatrick'
 email_subject = 'My updated resume'
